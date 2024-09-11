85c54f2686193a13d262340cde7fa43d3f27d4d1
Chang Liu <chang.liu0904@gmail.com>
2024-09-11 14:44:30 -0700

    connect to vercel pg

--- src/app/api/get-data/route.ts
+++ src/app/api/get-data/route.ts
@@ -1,15 +1,9 @@
 import { NextResponse } from 'next/server';
-import { Client } from 'pg';
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
-
-client.connect();
+import { sql } from '@vercel/postgres';
 
 export async function GET() {
   try {
-    const result = await client.query('SELECT temperature, humidity, timestamp FROM sensor_data ORDER BY timestamp DESC');
+    const result = await sql`SELECT temperature, humidity, timestamp FROM sensor_data ORDER BY timestamp DESC`;
     return NextResponse.json(result.rows);
   } catch (error) {
     return NextResponse.json({ error: 'Database error' }, { status: 500 });
--- src/app/api/post-data/route.ts
+++ src/app/api/post-data/route.ts
@@ -1,21 +1,13 @@
 import { NextResponse } from 'next/server';
-import { Client } from 'pg';
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
-
-client.connect();
+import { sql } from '@vercel/postgres';
 
 export async function POST(request: Request) {
   try {
     const { temperature, humidity } = await request.json();
     console.log("temp", temperature)
     console.log("humidity", humidity)
-    await client.query(
-      'INSERT INTO sensor_data (temperature, humidity, timestamp) VALUES ($1, $2, NOW())',
-      [temperature, humidity]
-    );
+    await sql`
+      INSERT INTO sensor_data (temperature, humidity, timestamp) VALUES (${temperature}, ${humidity}, NOW())`;
 
     return NextResponse.json({ message: 'Data stored successfully' });
   } catch (error) {
