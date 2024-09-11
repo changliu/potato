2b2e1e0fc8543c5399ad52b9dbc8ad36d6ce27fc
Chang Liu <chang.liu0904@gmail.com>
2024-09-11 14:50:44 -0700

    remove cache

--- src/app/api/get-data/route.ts
+++ src/app/api/get-data/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import { sql } from '@vercel/postgres';
+import { unstable_noStore } from 'next/cache';
 
 export async function GET() {
+    unstable_noStore();
   try {
     const result = await sql`SELECT temperature, humidity, timestamp FROM sensor_data ORDER BY timestamp DESC`;
     return NextResponse.json(result.rows);
